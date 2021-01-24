78ffcad17d4443b773761a939afb5b6143476d51
alexandrefaustino <alexandrefaculdade@gmail.com>
2021-01-24 18:09:36 -0300

    exercícios 01 Bloco18.3

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,15 +1,12 @@
-import React from 'react';
-
-import { Consumer } from './RedditContext';
-
-const Posts = () => (
-  <Consumer>
-    {({ posts }) => (
-      <ul>
-        {posts.map(({ id, title }) => <li key={id}>{title}</li>)}
-      </ul>
-    )}
-  </Consumer>
-);
+import React, { useContext } from 'react';
+import { Context } from './RedditContext';
+const Posts = () => {
+  const { posts } = useContext(Context);
+  return (
+    <ul>
+    {posts.map(({ id, title }) => <li key={id}>{title}</li>)}
+    </ul>   
+  )  
+};
 
 export default Posts;
